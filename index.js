06f2a2507904a98edb7f0d6e0797e34ac5de4cdf
entcheva <christina.entcheva@gmail.com>
2017-01-18 14:22:08 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,57 @@
+// var breakOut = (array, changeValue, stopValue) => {
+//   array.forEach((element, index, array) => {
+//
+//     if (element === stopValue) {
+//       break;
+//     } else {
+//       element === changeValue;
+//     }
+//
+//   });
+// };
+
+
+
+
+
+var breakOut = (array, changeValue, stopValue) => {
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i] === stopValue) {
+      break;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+
+  return array;
+
+};
+
+
+var keepGoing = (array, changeValue, skipValue) => {
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    } else {
+      array[i] = changeValue;
+    };
+  };
+
+  return array;
+
+};
+
+
+
+var findBy = (array, findFn) => {
+
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    };
+  };
+  
+};
